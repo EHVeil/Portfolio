afafadf7977b2ae3da94a63a28abc13e74478f94
Eli Veilleux <elibchv@gmail.com>
2023-05-17 16:36:02 -0700

    express server running

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './dist/src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-react', { targets: "defaults" }]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
